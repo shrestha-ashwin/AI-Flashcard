77d8dbbf9c90991d8249692d578f26f2a27b8a69
Ashwin Shrestha <shre.ashwin@gmail.com>
2024-09-24 12:43:34 -0500

    add card responsive design

--- components/Body.tsx
+++ components/Body.tsx
@@ -4,7 +4,7 @@ export default function Body() {
   return (
     <div className="bg-lightGrey flex justify-center">
       <div className=" flex flex-col gap-2 items-center py-2 w-[1000px] px-4">
-        <h2 className="font-bold text-[2rem] px-2 text-center mb-2">
+        <h2 className="font-bold text-[2rem] px-2 text-center mb-2 mt-4">
           Instant flashcards based on your topic choice
         </h2>
         <p className="mb-9">
@@ -26,7 +26,7 @@ export default function Body() {
               while.
             </p>
           </div>
-          <div className="card h-[17rem]">
+          <div className="card h-[17rem] card-3">
             {saving}
             <h3 className="mb-4 font-semibold">
               Step 3: Walla! Your cards are here!
