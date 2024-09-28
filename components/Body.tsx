7df9e57dffa6dba38e4418c833f026a5dbe52643
Ashwin Shrestha <shre.ashwin@gmail.com>
2024-09-27 22:08:02 -0500

    add outro section

--- components/Body.tsx
+++ components/Body.tsx
@@ -2,6 +2,7 @@ import Hero from "./Hero";
 import Intro from "./Intro";
 import Pricing from "./Pricing";
 import Footer from "./Footer";
+import Outro from "./Outro";
 
 export default function Body() {
   return (
@@ -10,6 +11,7 @@ export default function Body() {
         <Intro />
         <Hero />
         <Pricing />
+        <Outro />
       </div>
       <Footer />
     </div>
---
+++ components/Outro.tsx
@@ -0,0 +1,15 @@
+export default function Outro() {
+  return (
+    <div className="mx-auto max-w-[1000px] flex flex-col mb-24 bg-lightGrey px-6 py-16 rounded-[30px] items-center">
+      <h1 className="font-semibold text-3xl text-center mb-7">
+        Learn smarter, faster.
+      </h1>
+      <p className="text-center text-lightText mb-6">
+        Create your flashcards, and boost your knowledge.
+      </p>
+      <button className="bg-black text-borderGrey py-2 rounded-3xl px-5">
+        Get Started
+      </button>
+    </div>
+  );
+}
