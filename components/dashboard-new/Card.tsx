66475d2da8dd315ed4daced436272662a3ef36b0
Ashwin Shrestha <shre.ashwin@gmail.com>
2024-10-15 12:40:50 -0500

    fix responsive design autofill

--- components/dashboard-new/Card.tsx
+++ components/dashboard-new/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ cards }) {
   return (
-    <ul className="w-[800px] mx-auto px-4 gap-2 flashcard-grid">
+    <ul className="max-w-[800px] mx-auto px-4 flashcard-grid">
       {cards.map((card) => {
         console.log(card);
         return <li className="flashcard">{card.front}</li>;
