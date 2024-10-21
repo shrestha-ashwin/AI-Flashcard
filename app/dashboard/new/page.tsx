e3f8999c17e4e79965810cc40a9e3e7af6d230f6
Ashwin Shrestha <shre.ashwin@gmail.com>
2024-10-21 10:57:48 -0500

    make saving cards function

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -2,17 +2,16 @@
 import NavBar from "@/components/dashboard/NavBar";
 import { FlashCardContext } from "@/lib/flashCardContext";
 import Cards from "@/components/dashboard-new/Cards";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  const router = useRouter();
   const { flashCard } = FlashCardContext();
   if (flashCard.length == 0) {
     redirect("/dashboard");
   }
   console.log(flashCard);
   return (
-    <div>
+    <div className="cards-container">
       <NavBar />
       <Cards cards={flashCard} />
     </div>
--- components/dashboard-new/Cards.tsx
+++ components/dashboard-new/Cards.tsx
@@ -6,12 +6,32 @@ type FlashCard = {
 };
 
 export default function Cards({ cards }: { cards: FlashCard[] }) {
+  const data = async () => {
+    await fetch("/api/save-card", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cards),
+    });
+  };
+
   return (
-    <ul className="max-w-[800px] mx-auto px-4 flashcard-grid mb-[80px]">
-      {cards.map((card, id) => {
-        console.log(card);
-        return <Card key={id} card={card} />;
-      })}
-    </ul>
+    <div className="mb-[100px]">
+      <ul className="max-w-[800px] mx-auto px-4 flashcard-grid mb-[80px]">
+        {cards.map((card, id) => {
+          console.log(card);
+          return <Card key={id} card={card} />;
+        })}
+      </ul>
+      <div className="flex justify-center">
+        <button
+          className=" px-20 py-3 bg-[#000] text-white rounded-lg"
+          onClick={saveCards}
+        >
+          Save
+        </button>
+      </div>
+    </div>
   );
 }
