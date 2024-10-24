012837ef86603e1e4e3b8163bc25d45a2ca6e047
Ashwin Shrestha <shre.ashwin@gmail.com>
2024-10-24 15:38:56 -0500

    set up basic styles for save

--- components/dashboard/Hero.tsx
+++ components/dashboard/Hero.tsx
@@ -7,13 +7,23 @@ export default function Hero({ isCardSaved }) {
   const [flashCards, setFlashCards] = useState([]);
   console.log(isCardSaved);
 
+  setTimeout(() => {
+    isCardSaved = false;
+  }, 2000);
+
   return (
     <div className="">
-      <div className="max-w-[1000px] mx-auto px-10 ">
-        {isCardSaved ? <p>Saved</p> : ""}
+      <div className="max-w-[1000px] mx-auto px-10">
         <CreateCard />
         <section className="bg-white px-6 py-3">
-          <div className="mb-6">
+          <div className="mb-6 relative">
+            {isCardSaved ? (
+              <p className="inline-block px-8 py-2 bg-white font-medium absolute -top-[80px] left-[320px] rounded-md">
+                Saved
+              </p>
+            ) : (
+              ""
+            )}
             <h2 className="text-[#9c36b5] font-semibold text-2xl mb-1">
               Flashcards
             </h2>
