0494d9d501369036f10343edcd972e6bfbbc96cc
Ashwin Shrestha <shre.ashwin@gmail.com>
2025-02-11 09:40:32 -0600

    fix dashboard error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import NavBar from "@/components/dashboard/NavBar";
 import Hero from "@/components/dashboard/Hero";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function Page() {
   const searchParams = useSearchParams();
+  const [isCardSaved, setIsCardSaved] = useState(false);
 
-  const isCardSaved = searchParams.get("status") === "true";
-  console.log(isCardSaved);
+  useEffect(() => {
+    setIsCardSaved(searchParams.get("status") === "true");
+  }, [searchParams]);
 
   return (
     <div className="bg-[#f1f3f5] h-[100vh] relative ">
